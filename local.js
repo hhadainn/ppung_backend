a2829ef23759c4fd061d2d7b610a9209ab6230e0
hhadainn <hhadainn@gmail.com>
2025-07-18 14:15:03 +0900

    fix

--- local.js
+++ local.js
@@ -1,1 +1,13 @@
-const app = require('./app');
+const mongoose = require('mongoose');
+
+// MongoDB URI (Atlas 또는 로컬)
+const MONGODB_URI = 'mongodb+srv://hhadainn:ukubXeiXys8C7K2F@cluster0.86tbade.mongodb.net/';
+// 또는
+// const MONGODB_URI = 'mongodb://localhost:27017/myDB';
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('✅ MongoDB connected!'))
+.catch((err) => console.error('❌ MongoDB connection error:', err));
---
+++ services/db.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const MongoClient = require("mongodb").MongoClient;
+const { ObjectId } = require("mongodb");
+
+const uri = process.env.MONGODB_URI;
+let cachedDb = null;
+let cachedDbLocal = null;
+
+async function connectToDatabase() {
+    if (cachedDb) {
+        return cachedDb;
+    }
+    const client = await MongoClient.connect(uri);
+    const db = client.db('service');
+    cachedDb = db;
+    return db;
+}
+
+async function collection(collName) {
+    const db = await connectToDatabase();
+    return db.collection(collName)
+}
+module.exports = {collection}
+
+// const test = async() => {
+//     try{
+//         const coll = await collection("user")
+//         const userTest = await coll.findOne({name:'hhand'})
+//         console.log(userTest)
+//     }
+//     catch(e){
+//         console.log(e)
+//     }
+// }
+// test()
