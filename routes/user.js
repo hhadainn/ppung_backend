5c791976c2a625a049c4b2ac3253d4a939d56a8a
KaistHyunho <nhh123@kaist.ac.kr>
2025-07-22 01:04:05 +0900

    fix

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = express.Router()
-router.get('/test', async(req, res) => {
+router.post('/test', async(req, res) => {
+    try{
+        const {type, type2} = req.query
+		const {data} = req.body
+		console.log(type, type2, data)
+        return res.status(200).send({message:'다인'})
+    }
+    catch(e){
+        console.log(e)
+    }
+})
+router.get('/test2', async(req, res) => {
+    try{
+        console.log(123)
+        return res.status(200).send({message:'success'})
+    }
+    catch(e){
+        console.log(e)
+    }
+})
+router.get('/test3', async(req, res) => {
     try{
         console.log(123)
         return res.status(200).send({message:'success'})
